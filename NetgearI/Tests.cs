1be61577ee9c739f3affe52fb183de6b7a78c92b
JuneCho23 <junecho.ucb@gmail.com>
2017-03-22 10:31:43 -0700

    created Regions

--- NetgearI/Tests.cs
+++ NetgearI/Tests.cs
@@ -35,28 +35,24 @@ namespace NetgearI
 		[Test]
 		public void FirstTest()
 		{
+			#region LoginTests
 			app.Tap("main_login_netgear");
 			app.Screenshot("Let's start by Tapping on the 'Sign In' Button");
-
 			app.Tap("login_username");
 			app.Screenshot("Then we Tapped on the 'Username' Text Field");
-
 			app.EnterText("breckwight@gmail.com");
 			app.Screenshot("Next we entered our e-mail, 'breckwight@gmail.com'");
-
 			app.DismissKeyboard();
 			app.Screenshot("Dismissed Keyboard");
-
 			app.Tap("login_password");
 			app.Screenshot("We Tapped on the 'Password' Login Text Field");
-
 			app.EnterText("Xamarin123");
 			app.Screenshot("Next we entered in our password, 'Xamarin123'");
-
 			app.DismissKeyboard();
 			app.Screenshot("Dismissed Keyboard");
 			app.Tap("login");
 			app.Screenshot("Then we Tapped on the 'Login Button'");
+			#endregion
 
 
 			app.Tap("add_new");
